c57055c07cd542fef9d269d1b98e76be9e1ab765
Mads Engel Lundt <madslundt@live.dk>
2019-05-16 23:20:20 +0200

    Added tests

---
+++ src/__tests__/components/Counter/Counter.tsx
@@ -0,0 +1,79 @@
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import React from "react";
+import {render, fireEvent, cleanup} from "react-testing-library";
+import Counter from "../../../components/Counter";
+
+afterEach(cleanup);
+
+interface IProps {
+    initialValue: number
+}
+
+test("Have initial value", () => {
+    const expectedInitialValue = 10;
+    const { getByText } = render(
+        <Counter initialValue={expectedInitialValue} />
+    );
+
+    expect(getByText(expectedInitialValue.toString())).toBeInTheDocument();
+});
+
+test("Have initial value incremented", () => {
+    const expectedInitialValue = 10;
+    const { getByText } = render(
+        <Counter initialValue={expectedInitialValue} />
+    );
+
+    const increment = getByText("Increment");
+
+    fireEvent.click(increment);
+    expect(getByText((expectedInitialValue + 1).toString())).toBeInTheDocument();
+
+    fireEvent.click(increment);
+    expect(getByText((expectedInitialValue + 2).toString())).toBeInTheDocument();
+});
+
+test("Have initial value decremented", () => {
+    const expectedInitialValue = 10;
+    const { getByText } = render(
+        <Counter initialValue={expectedInitialValue} />
+    );
+
+    const decrement = getByText("Decrement");
+
+    fireEvent.click(decrement);
+    expect(getByText((expectedInitialValue - 1).toString())).toBeInTheDocument();
+
+    fireEvent.click(decrement);
+    expect(getByText((expectedInitialValue - 2).toString())).toBeInTheDocument();
+});
+
+test("Have initial value incremented and decremented", () => {
+    const expectedInitialValue = 10;
+    const { getByText } = render(
+        <Counter initialValue={expectedInitialValue} />
+    );
+
+    const increment = getByText("Increment");
+    const decrement = getByText("Decrement");
+
+    fireEvent.click(decrement);
+    expect(getByText((expectedInitialValue - 1).toString())).toBeInTheDocument();
+
+    fireEvent.click(decrement);
+    expect(getByText((expectedInitialValue - 2).toString())).toBeInTheDocument();
+
+    fireEvent.click(increment);
+    expect(getByText((expectedInitialValue - 1).toString())).toBeInTheDocument();
+
+    fireEvent.click(increment);
+    expect(getByText(expectedInitialValue.toString())).toBeInTheDocument();
+
+    fireEvent.click(increment);
+    expect(getByText((expectedInitialValue + 1).toString())).toBeInTheDocument();
+
+    fireEvent.click(decrement);
+    expect(getByText(expectedInitialValue.toString())).toBeInTheDocument();
+});
---
+++ src/__tests__/components/Counter/useCounter.tsx
@@ -0,0 +1,96 @@
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import React from "react";
+import {render, fireEvent, cleanup} from "react-testing-library";
+import { useCounter } from "../../../components/Counter";
+
+afterEach(cleanup);
+
+interface IProps {
+    initialValue: number
+}
+
+const Component = (props: IProps) => {
+    const {
+        count,
+        increment,
+        decrement
+    } = useCounter(props.initialValue);
+
+    return (
+        <div>
+            <p data-testid="count">{count}</p>
+
+            <button type="button" onClick={() => increment()} data-testid="increment" />
+            <button type="button" onClick={() => decrement()} data-testid="decrement" />
+        </div>
+    );
+}
+
+test("Have initial value", () => {
+    const expectedInitialValue = 10;
+    const { getByTestId } = render(
+        <Component initialValue={expectedInitialValue} />
+    );
+
+    expect(getByTestId("count").textContent).toBe(expectedInitialValue.toString());
+});
+
+test("Have initial value incremented", () => {
+    const expectedInitialValue = 10;
+    const { getByTestId } = render(
+        <Component initialValue={expectedInitialValue} />
+    );
+
+    const increment = getByTestId("increment");
+
+    fireEvent.click(increment);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue + 1).toString());
+
+    fireEvent.click(increment);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue + 2).toString());
+});
+
+test("Have initial value decremented", () => {
+    const expectedInitialValue = 10;
+    const { getByTestId } = render(
+        <Component initialValue={expectedInitialValue} />
+    );
+
+    const decrement = getByTestId("decrement");
+
+    fireEvent.click(decrement);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue - 1).toString());
+
+    fireEvent.click(decrement);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue - 2).toString());
+});
+
+test("Have initial value incremented and decremented", () => {
+    const expectedInitialValue = 10;
+    const { getByTestId } = render(
+        <Component initialValue={expectedInitialValue} />
+    );
+
+    const increment = getByTestId("increment");
+    const decrement = getByTestId("decrement");
+
+    fireEvent.click(decrement);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue - 1).toString());
+
+    fireEvent.click(decrement);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue - 2).toString());
+
+    fireEvent.click(increment);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue - 1).toString());
+
+    fireEvent.click(increment);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue).toString());
+
+    fireEvent.click(increment);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue + 1).toString());
+
+    fireEvent.click(decrement);
+    expect(getByTestId("count").textContent).toBe((expectedInitialValue).toString());
+});
